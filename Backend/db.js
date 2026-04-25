4ce4673837f92bfe1b3de023b2b68e365c04042f
younes971 <younesb@metropolia.fi>
2026-04-25 14:58:15 +0300

    Update

--- Backend/db.js
+++ Backend/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 const db = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "1234", // your password
+  password: "1234",
   database: "marketplace",
 });
 
